e137bc555c26b752b8d111194007b9c7c488273c
kaoutar-12 <kmouradi@student.1337.ma>
2024-01-25 17:29:40 +0100

    todo done

--- todo_list_v2/src/componant/EditTodoForm.jsx
+++ todo_list_v2/src/componant/EditTodoForm.jsx
@@ -1,23 +1,28 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
-export const EditTodoForm =({editTodo, task}) => {
-    const [value, setValue] = useState("")
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        editTodo(value, task.id)
-        setValue("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value !== "") {
+      editTodo(value, task.id);
+      setValue("");
     }
+  };
 
-    return (
-        <form className="TodoForm" onSubmit={handleSubmit}>
-            <input type="text" className="todo-input" value={value}
-            placeholder="Update Task!" onChange=
-            {(e) => setValue(e.target.value)}/>
-            <button type="submit" className="todo-btn">
-                Update Task
-            </button>
-        </form>
-    )
-}
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="Update Task!"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
