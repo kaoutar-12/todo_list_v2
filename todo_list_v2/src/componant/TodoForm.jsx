2a63012aace5ffb7f5dc212eaffd509b090bfe21
kaoutar-12 <kmouradi@student.1337.ma>
2024-01-25 13:01:11 +0100

    todo

---
+++ todo_list_v2/src/componant/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export const TodoForm =() => {
+    return (
+        <form className="TodoForm">
+            <input type="text" className="todo-input" 
+            placeholder="what is the task today?" />
+            <button type="submit" className="todo-btn">
+                Add Task
+            </button>
+        </form>
+    )
+}
